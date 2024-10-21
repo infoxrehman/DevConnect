50ac53d58f72125d617e956680be30c10fa28f54
Abdullah Shaikh <shaikh.abd24@gmail.com>
2024-10-21 18:23:29 +0530

    Added Edge Runtime, nextconfig updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+import { Turtle } from 'lucide-react';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+// ADDED ON 21-10-2024 by watching Elliott Chong video
+    typescript: {
+        ignoreBuildErrors:true,
+    },
+    eslint:{
+        ignoreDuringBuilds: true,
+    },
+};
+
 
 export default nextConfig;
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,7 @@ import NextAuth from "next-auth";
 import { authConfig } from "@/lib/auth";
 
 const handler = NextAuth(authConfig);
+// ADDED ON 21-10-2024 by watching Elliott Chong video
+export const runtime = 'egde'
 
 export { handler as GET, handler as POST };
